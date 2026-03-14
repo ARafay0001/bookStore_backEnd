e833c4d8d1877fc5a20defa8569c8f43045ca8af
arafa0001 <abdulrafayimran4@gmail.com>
2026-03-14 19:20:10 +0500

    node

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import {port} from "./config.js";
 
 const app = express();
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
 app.get("/", (req, res) => {
     res.send("Hello, World!");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
