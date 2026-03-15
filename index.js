1deeff17af91deaec9e8c1c06766d0522cc02202
arafa0001 <abdulrafayimran4@gmail.com>
2026-03-15 20:00:05 +0500

    mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {port} from "./config.js";
-
+import {port, mongoDBURL02 } from "./config.js";
+import mongoose from "mongoose"
 const app = express();
 
 app.get("/", (req, res) => {
@@ -8,6 +8,17 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(port, () => {
+
+
+mongoose.connect(mongoDBURL02)
+.then(()=>{
+    console.log("Connected to MongoDB successfully!");
+
+    
+    app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+})
+.catch((value) => {
+    console.error("Error connecting to MongoDB:", value);
+})
